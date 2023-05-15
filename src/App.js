c15873f3538ee0f5d745efafdc209202f0ba6b22
Mukul Ghanghas <partimag55@gmail.com>
2023-05-15 19:26:08 +0530

    add it alt

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import React, {useState} from "react";
-import data from './data'
+import React, { useState } from "react";
+import data from "./data";
 import Tours from "./components/Tours";
 
 const App = () => {
-
   const [tours, setTours] = useState(data);
 
   function removeTour(id) {
-    const newTours = tours.filter(tour => tour.id !== id);
+    const newTours = tours.filter((tour) => tour.id !== id);
     setTours(newTours);
   }
 
-  if(tours.length === 0) {
+  if (tours.length === 0) {
     return (
-        <div className="refresh">
-          <h2>No Tours Left</h2>
-          <button className="btn-white" onClick={() => setTours(data)}>
-            Refresh
-          </button>
-        </div>
+      <div className="refresh">
+        <h2>No Tours Left</h2>
+        <button className="btn-white" onClick={() => setTours(data)}>
+          Refresh
+        </button>
+      </div>
     );
   }
 
@@ -26,7 +25,7 @@ const App = () => {
     <div className="App">
       <Tours tours={tours} removeTour={removeTour}></Tours>
     </div>
-  )
+  );
 };
 
 export default App;
